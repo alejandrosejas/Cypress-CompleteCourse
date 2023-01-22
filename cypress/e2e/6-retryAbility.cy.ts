d200db007d6e95bf9a63edd4a3536d103d2524e0
alejandrosejas <alejandrosejas@outlook.com>
2023-01-22 12:47:16 -0400

    6-retryAbility(automatic waits)

---
+++ cypress/e2e/6-retryAbility.cy.ts
@@ -0,0 +1,27 @@
+Cypress.config('defaultCommandTimeout', 16000)
+
+describe('Retry Ability Demo', () => {
+   it('Visits a website with a Delay', () => {
+      cy.visit('/loaddelay', { timeout: 0 })
+   })
+   it('Visits a website with a Delay', () => {
+      cy.visit('/loaddelay')
+   })
+
+   // 'defaultCommandTimeOut' added to pass this Test
+   it('Client-side delay', () => {
+      cy.visit('/clientdelay')
+      cy.get('#ajaxButton').click()
+      cy.get('.bg-success').should(
+         'have.text',
+         'Data calculated on the client side.'
+      )
+   })
+
+   // Adds a timeout for a specific selector
+   it('Progress Bar Scenario', () => {
+      cy.visit('/progressbar')
+      cy.get('#startButton').click()
+      cy.get('#progressBar', { timeout: 30000 }).should('have.text', '100%')
+   })
+})
